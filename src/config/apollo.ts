5ce380ef413bade0d8605b43aed90bb365fa9468
wizkhalifa64 <sona95986@gmail.com>
2024-03-26 23:57:59 +0530

    added url

--- src/config/apollo.ts
+++ src/config/apollo.ts
@@ -1,9 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { AppConfig } from "./appConfig";
 
 export const client = (token: string) =>
   new ApolloClient({
-    uri: "http://localhost:8000/rust-graphql",
+    uri: process.env.NEXT_PUBLIC_API,
     cache: new InMemoryCache(),
     headers: {
       authorization: token,
